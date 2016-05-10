61b1c3e9e1012fdd1952aaa4d4cbeb0578ea5f24
lievi <lievi.silva@gmail.com>
2016-05-10 20:58:18 -0300

    First Commit

---
+++ EP03/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace EP03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Controlar = new System.Windows.Forms.Button();
+            this.Pausar = new System.Windows.Forms.Button();
+            this.Reiniciar = new System.Windows.Forms.Button();
+            this.Liberar = new System.Windows.Forms.Button();
+            this.InicProc = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Controlar
+            // 
+            this.Controlar.Location = new System.Drawing.Point(51, 90);
+            this.Controlar.Name = "Controlar";
+            this.Controlar.Size = new System.Drawing.Size(172, 23);
+            this.Controlar.TabIndex = 2;
+            this.Controlar.Text = "Controlar Trem";
+            this.Controlar.UseVisualStyleBackColor = true;
+            this.Controlar.Click += new System.EventHandler(this.Iniciar_Click);
+            // 
+            // Pausar
+            // 
+            this.Pausar.Location = new System.Drawing.Point(51, 177);
+            this.Pausar.Name = "Pausar";
+            this.Pausar.Size = new System.Drawing.Size(172, 23);
+            this.Pausar.TabIndex = 4;
+            this.Pausar.Text = "Pausar Movimento";
+            this.Pausar.UseVisualStyleBackColor = true;
+            this.Pausar.Click += new System.EventHandler(this.Pausar_Click);
+            // 
+            // Reiniciar
+            // 
+            this.Reiniciar.Location = new System.Drawing.Point(51, 133);
+            this.Reiniciar.Name = "Reiniciar";
+            this.Reiniciar.Size = new System.Drawing.Size(172, 23);
+            this.Reiniciar.TabIndex = 3;
+            this.Reiniciar.Text = "Reiniciar Movimento";
+            this.Reiniciar.UseVisualStyleBackColor = true;
+            this.Reiniciar.Click += new System.EventHandler(this.Reiniciar_Click);
+            // 
+            // Liberar
+            // 
+            this.Liberar.Location = new System.Drawing.Point(51, 219);
+            this.Liberar.Name = "Liberar";
+            this.Liberar.Size = new System.Drawing.Size(172, 23);
+            this.Liberar.TabIndex = 5;
+            this.Liberar.Text = "Liberar Controle";
+            this.Liberar.UseVisualStyleBackColor = true;
+            this.Liberar.Click += new System.EventHandler(this.Liberar_Click);
+            // 
+            // InicProc
+            // 
+            this.InicProc.Location = new System.Drawing.Point(197, 30);
+            this.InicProc.Name = "InicProc";
+            this.InicProc.Size = new System.Drawing.Size(75, 23);
+            this.InicProc.TabIndex = 1;
+            this.InicProc.Text = "Iniciar Proc.";
+            this.InicProc.UseVisualStyleBackColor = true;
+            this.InicProc.Click += new System.EventHandler(this.InicProc_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(71, 30);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(113, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 267);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.InicProc);
+            this.Controls.Add(this.Liberar);
+            this.Controls.Add(this.Reiniciar);
+            this.Controls.Add(this.Pausar);
+            this.Controls.Add(this.Controlar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Controlar;
+        private System.Windows.Forms.Button Pausar;
+        private System.Windows.Forms.Button Reiniciar;
+        private System.Windows.Forms.Button Liberar;
+        private System.Windows.Forms.Button InicProc;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ EP03/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibUDP;
+
+namespace EP03
+{
+    public partial class Form1 : Form
+    {
+        private int seqLocal = 0;
+        private int seqExt = 0;
+        private byte id;
+        private bool critic = false;
+        private bool[] Aguardando = { false, false };
+        private int[] repliesEspera = new int[2];
+        int temp = 0;
+        int temp2 = 0;
+        UDPSocket socket;
+        public Form1()
+        {
+            InitializeComponent();
+            Controlar.Enabled = false;
+            Pausar.Enabled = false;
+            Reiniciar.Enabled = false;
+            Liberar.Enabled = false;
+        }
+
+        private void MensagemRecebida(byte[] buffer, int size, string ip, int port)
+        {
+            if (buffer[0] == 0 && buffer[1] != id)
+            {
+                if (seqExt < buffer[2])
+                    seqExt = buffer[2];
+
+                if (critic == true)
+                {
+                    //Erro
+                    if (repliesEspera[0] != port && repliesEspera[1] != port)
+                    {
+                        repliesEspera[temp2] = port;
+                        temp2++;
+                    }
+                }
+                else if (Aguardando[0] == false && Aguardando[1] == false)
+                {
+                    //Enviar
+                    buffer[0] = 1;
+                    socket.Send(buffer, ip, port);
+                }
+                else if (seqLocal < buffer[2])
+                {
+                    //Erro
+                    if (repliesEspera[0] != port && repliesEspera[1] != port)
+                    {
+                        repliesEspera[temp2] = port;
+                        temp2++;
+                    }
+                }
+                else if (seqLocal > buffer[2])
+                {
+                    //Enviar
+                    buffer[0] = 1;
+                    socket.Send(buffer, ip, port);
+                }
+                else if (id < buffer[1])
+                {
+                    //Erro
+                    if (repliesEspera[0] != port && repliesEspera[1] != port)
+                    {
+                        repliesEspera[temp2] = port;
+                        temp2++;
+                    }
+                }
+                else
+                {
+                    //Enviar
+                    buffer[0] = 1;
+                    socket.Send(buffer, ip, port);
+                }
+            }
+            else if (buffer[0] == 1)
+            {
+                //Necessário verificar o reply de todos os processos que foram enviaram
+                Aguardando[temp] = false;
+                if (Aguardando[0] == false && Aguardando[1] == false)
+                {
+                    critic = true;
+                    label3.Text = "Pode";
+                    temp = 0;
+                    Liberar.Enabled = true;
+                    Pausar.Enabled = true;
+                    Reiniciar.Enabled = true;
+                }
+                else
+                    temp++;
+            }
+        }
+
+        private void Iniciar_Click(object sender, EventArgs e)
+        {
+            //Atualizando relógio
+            if (seqLocal <= seqExt)
+                seqLocal = seqExt + 1;
+
+            //Enviando request com minha sequencia(relógio) para TODOS os processos
+
+            byte[] msg = { 0, id, Convert.ToByte(seqLocal) };
+
+            //Temporário
+            if(Aguardando[0] != true && Aguardando[1] != true)
+            {
+                Aguardando[0] = true;
+                Aguardando[1] = true;
+                socket.Send(msg, "127.0.0.1", 12001);
+                socket.Send(msg, "127.0.0.1", 12002);
+                socket.Send(msg, "127.0.0.1", 12003);
+            }
+        }
+
+        private void InicProc_Click(object sender, EventArgs e)
+        {           
+            id = Convert.ToByte(txtID.Text);
+            socket = new UDPSocket(MensagemRecebida, 12000 + Convert.ToInt16(txtID.Text));
+            Controlar.Enabled = true;
+            InicProc.Enabled = false;
+        }
+
+        private void Reiniciar_Click(object sender, EventArgs e)
+        {
+            if (critic)
+            {
+                byte[] batata = { 0 };
+                socket.Send(batata, "127.0.0.1", 7200);
+            }
+        }
+
+        private void Pausar_Click(object sender, EventArgs e)
+        {
+            if (critic)
+            {
+                byte[] batata = { 1 };
+                socket.Send(batata, "127.0.0.1", 7200);
+            }
+        }
+
+        private void Liberar_Click(object sender, EventArgs e)
+        {
+            critic = false;
+            label3.Text = "Liberado";
+            byte[] batata = { 1 };
+
+            if (repliesEspera[0] != 0)
+                socket.Send(batata, "127.0.0.1", repliesEspera[0]);
+            if (repliesEspera[1] != 0)
+                socket.Send(batata, "127.0.0.1", repliesEspera[1]);
+
+            temp2 = 0;
+            temp = 0;
+            repliesEspera[0] = 0;
+            repliesEspera[1] = 0;
+
+            Liberar.Enabled = false;
+            Pausar.Enabled = false;
+            Reiniciar.Enabled = false;
+        }
+    }
+}
